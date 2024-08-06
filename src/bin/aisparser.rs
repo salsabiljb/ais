480bccfe21080b1d9cb3e5364a94f29b4b3d30b6
salsabiljb <salsabil.jaballah@etudiant-enit.utm.tn>
2024-08-06 11:22:02 +0100

    feat: add support for tag block + doc
    add documentation and direct use of fn decode message with passed argument

--- src/bin/aisparser.rs
+++ src/bin/aisparser.rs
@@ -1,4 +1,4 @@
-use ais::decoders::utils::{decode_from_file, decode_from_tcp, decode_from_udp};
+use ais::{decode, decode_from_file, decode_from_tcp, decode_from_udp};
 use clap::{Arg, Command};
 use std::error::Error;
 
@@ -28,6 +28,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .value_name("PATH")
                 .help("Path to the file to read AIS messages from"),
         )
+        .arg(
+            Arg::new("message")
+                .short('m')
+                .long("message")
+                .value_name("AIS_MESSAGE")
+                .help("A single AIS message to decode"),
+        )
         .get_matches();
 
     if let Some(address) = matches.get_one::<String>("udp") {
@@ -36,6 +43,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         decode_from_tcp(address).await?;
     } else if let Some(path) = matches.get_one::<String>("file") {
         decode_from_file(path).await?;
+    } else if let Some(message) = matches.get_one::<String>("message") {
+        match decode(message.as_bytes()) {
+            Ok(parsed_message) => println!("Parsed AIS Message: {:?}", parsed_message),
+            Err(e) => eprintln!("Failed to parse AIS message: {}", e),
+        }
     } else {
         eprintln!("No valid command provided.");
     }
